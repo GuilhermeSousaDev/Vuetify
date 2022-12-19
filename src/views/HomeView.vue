f1bf3c4e2d4e50183b35940c9d22e3e53104dd27
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-12-19 09:53:10 -0300

    Task List View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,35 @@
 <template>
-  <hello-world />
+  <div>
+    <v-list
+      flat
+      subheader
+      three-line
+    >
+      <v-subheader>General</v-subheader>
+
+      <v-list-item-group
+        v-model="settings"
+        multiple
+        active-class=""
+      >
+        <Task />
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Task from '@/components/Tasks/Task.vue';
 
   export default {
     name: 'Home',
-
+    data() {
+      return {
+        settings: [],
+      }
+    },
     components: {
-      HelloWorld,
+      Task
     },
   }
 </script>
